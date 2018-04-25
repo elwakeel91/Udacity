89a0f82fa9280929edca820aeffee6b72c80190b
Kareem El-Wakeel <elwakeel91@gmail.com>
2018-04-25 19:24:34 +0100

    Add Football_Scorer app

---
+++ Android Development for Beginners/FootballScorer/app/src/main/java/com/example/android/footballscorer/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.android.footballscorer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int scoreTeamA;
+    private int scoreTeamB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+    }
+
+    public void goalA (View view) {
+
+        // Get the EditText object that has the name of the scorer
+        EditText editText = findViewById(R.id.player_name_team_A);
+
+        // If the text field is empty, don't do anything
+        if (editText.getText().toString().equals(""))
+            return;
+
+        // Update the ScoreBoard
+        scoreTeamA++;
+        TextView score = findViewById(R.id.score_team_A);
+        score.setText(String.valueOf(scoreTeamA));
+
+        // Get the name of the scorer from the EditText view
+        String name = editText.getText().toString();
+        // Remove the name from the EditText view
+        editText.setText("");
+
+        // Add a goal to the Team score board
+        addGoal(view, name);
+    }
+
+    public void goalB (View view) {
+
+        // Get the EditText object that has the name of the scorer
+        EditText editText = findViewById(R.id.player_name_team_B);
+
+        // If the text field is empty, don't do anything
+        if (editText.getText().toString().equals(""))
+            return;
+
+        // Update the ScoreBoard
+        scoreTeamB++;
+        TextView score = findViewById(R.id.score_team_B);
+        score.setText(String.valueOf(scoreTeamB));
+
+        // Get the name of the scorer from the EditText view
+        String name = editText.getText().toString();
+        // Remove the name from the EditText view
+        editText.setText("");
+
+        // Add a goal to the Team score board
+        addGoal(view, name);
+    }
+
+    private void addGoal(View view, String name)
+    {
+        // Set the parent as the Team Linear Layout
+        LinearLayout teamLayout = (LinearLayout) view.getParent();
+
+        // Create a new custom view using the scorer_layout.xml file and make it a child of the Main Layout
+        View scorerView = LayoutInflater.from(this).inflate(R.layout.scorer_layout, teamLayout, false);
+        // Find the TextView holding the name of the scorer
+        TextView nameView = scorerView.findViewById(R.id.name);
+        // Set the name
+        nameView.setText(name);
+
+        // Add the custom view to the Main Layout
+        teamLayout.addView(scorerView);
+    }
+}
